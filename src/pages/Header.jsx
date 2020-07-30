4c2e0daac7ec05874c8a8a3210553409fcad0f62
Harinder kumar <harinderkumar1806@gmail.com>
2020-07-30 16:59:13 +0530

    changes in image logo

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import BackVideo from '../images/v5.mp4'
-import Logo from '../images/logo.jpeg'
+import Logo from '../images/mainLO.png'
 class Header extends Component {
   componentDidMount () {
     document.getElementById('background-video').play();
