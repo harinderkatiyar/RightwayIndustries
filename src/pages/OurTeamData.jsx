090ceb325154df1fbd0fe4eab8befaa1502f9879
Harinder kumar <harinderkumar1806@gmail.com>
2020-08-08 18:00:14 +0530

    card info changed  has been added sucessfully

--- src/pages/OurTeamData.jsx
+++ src/pages/OurTeamData.jsx
@@ -2,22 +2,22 @@
 
 const OurTeamData =
 [{
-        "text":"Lorem Ipsum",
+        "text":"Comming Soon",
         "image": "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
        
     },
     {
-        "text":"Lorem Ipsum",
+        "text":"Comming Soon",
         "image": "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
       
     },
     {
-        "text":"Lorem Ipsum",
+        "text":"Comming Soon",
         "image": "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
        
     },
     {
-        "text":"Lorem Ipsum",
+        "text":"Comming Soon",
         "image": "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
        
     }
