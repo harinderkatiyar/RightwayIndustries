846c955a54499247ee54dbfd5ce1262480ff0a4c
Harinder kumar <harinderkumar1806@gmail.com>
2020-08-08 21:28:48 +0530

    added coming soon text sucessfully

--- src/pages/OurTeamData.jsx
+++ src/pages/OurTeamData.jsx
@@ -2,22 +2,22 @@
 
 const OurTeamData =
 [{
-        "text":"Comming Soon",
+        "text":"Coming Soon",
         "image": "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
        
     },
     {
-        "text":"Comming Soon",
+        "text":"Coming Soon",
         "image": "https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
       
     },
     {
-        "text":"Comming Soon",
+        "text":"Coming Soon",
         "image": "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
        
     },
     {
-        "text":"Comming Soon",
+        "text":"Coming Soon",
         "image": "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
        
     }
